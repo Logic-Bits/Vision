f4d3c57d4e92ade4b33b39d8453358f738c692ff
Logic-Bits <dennes.schaeffler@googlemail.com>
2016-12-07 12:27:11 +0100

    added packages, renamed name, added stuff to server

--- server.js
+++ server.js
@@ -12,6 +12,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
 
+
+var pjax 	= require('express-pjax');	//express pjax (partial reloads)
+var hbs 	= require('express-hbs');	//express handlebars
+var moment	= require('moment');		//moment date formatting lib
+
+//pjax middleware for partials
+app.use(pjax());
+
+//send session info to handlebars, check OS used to send correct stylesheet
+app.use(function(req, res, next){
+
+	var ua = req.headers['user-agent'];
+	req.session.isAndroid = (ua.match(/Android/i) != null);
+	req.session.isIos = (ua.match(/iPhone|iPad|iPod/i) != null);
+	req.session.isDev = (process.env.NODE_ENV !='production');
+	req.session.test = (process.env.NODE_ENV);
+
+	res.locals.session = req.session;
+
+	next();
+});
+
 // use JWT auth to secure the api
 app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate',
     '/api/users/register'] }));
@@ -27,6 +49,37 @@ app.use('/api/usecases', require('./controllers/api/usecases.controller'));
 app.get('/', function (req, res) {
     return res.redirect('/app');
 });
+
+//helper to get the icon for a item type
+hbs.registerHelper("getIconForType", function(type) {
+	return bc.getIconForType(type);
+});
+
+//helper to get the stylesheet for the current user agent
+hbs.registerHelper("getCSSforOS", function(session) {
+	var bootCardsBase = "//cdnjs.cloudflare.com/ajax/libs/bootcards/1.1.2/css/";
+	if (session.isAndroid) {
+		return '<link href="' + bootCardsBase + 'bootcards-android.min.css" rel="stylesheet" type="text/css" />';
+	} else if (session.isIos) {
+		return '<link href="' + bootCardsBase + 'bootcards-ios.min.css" rel="stylesheet" type="text/css" />';
+	} else {
+		return '<link href="' + bootCardsBase + 'bootcards-desktop.min.css" rel="stylesheet" type="text/css" />';
+	}
+});
+
+hbs.registerHelper("isMobile", function(session) {
+	return '<script>var isDesktop = ' + (!session.isIos && !session.isAndroid) + ';</script>';
+});
+
+//helper to get the app version
+hbs.registerHelper("getAppVersion", function() {
+	return pjson.version;
+});
+
+// // development only
+// if ('development' == app.get('env')) {
+//   app.use(express.errorHandler());
+// }
 
 // start server
 var server = app.listen(3000, function () {
