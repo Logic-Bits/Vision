398b3bce6a5d9ae14e2feafc9bcca8ec6fa4e072
Logic-Bits <dennes.schaeffler@googlemail.com>
2016-12-23 16:54:31 +0100

    formatted code, added description, test design for linked spez.

--- app/qm/usecases.controller.js
+++ app/qm/usecases.controller.js
@@ -1,80 +1,82 @@
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular
-        .module('app')
-        .controller('Qm.UseCasesController', Controller);
+  angular
+    .module('app')
+    .controller('Qm.UseCasesController', Controller);
 
-    function Controller($window, UseCaseService, FlashService) {
-        var vm = this;
-        vm.usecase = null;
-        vm.createUseCase = createUseCase;
-        vm.updateUseCase= updateUseCase;
-        vm.select = select;
-        vm.deleteUseCase = deleteUseCase;
+  function Controller($window, UseCaseService, FlashService) {
+    var vm = this;
+    vm.usecase = null;
+    vm.createUseCase = createUseCase;
+    vm.updateUseCase = updateUseCase;
+    vm.select = select;
+    vm.deleteUseCase = deleteUseCase;
 
-        initController();
+    initController();
 
-        function initController() {
-            UseCaseService.GetAll().then(function (ucs){
-                  vm.usecases = ucs;
+    function initController() {
+      UseCaseService.GetAll().then(function(ucs) {
+        vm.usecases = ucs;
 
-                  console.log("found usecases: " + vm.usecases.length);
+        console.log("found usecases: " + vm.usecases.length);
 
-                  if(vm.usecases.length > 0)
-                  {
-                    vm.usecase = vm.usecases[0];
-                  }
-            });
+        if (vm.usecases.length > 0) {
+          vm.usecase = vm.usecases[0];
         }
-
-        function updateUseCase() {
-            UseCaseService.Update(vm.usecase)
-                .then(function () {
-                    FlashService.Success('Use Case updated');
-
-                    //now update the item in the master list. no full refresh needed
-                    for (var i=0; i < vm.usecases.length; i++) {
-                        if (vm.usecases[i]._id === vm.usecase._id) {
-                            vm.usecases[i] = vm.usecase;
-                            break;
-                        }
-                    }
-                })
-                .catch(function (error) {
-                    FlashService.Error(error);
-                });
-        }
-
-        function createUseCase() {
-          UseCaseService.Create(vm.newusecase)
-              .then(function () {
-                  FlashService.Success('Use Case created');
-                  initController();
-                  //todo refresh
-              })
-              .catch(function (error) {
-                  FlashService.Error(error);
-              });
-        }
-
-        function select(usecase){
-          UseCaseService.GetById(usecase._id).then(function(uc){
-            vm.usecase = uc;
-          });
-        }
-
-        function deleteUseCase(usecase){
-          UseCaseService.Delete(usecase._id).then(function () {
-              FlashService.Success('Use Case deleted');
-              $('#confirm-delete').modal('hide');
-              initController();
-
-          })
-          .catch(function (error) {
-              FlashService.Error(error);
-          });
-        }
+      });
     }
+
+    function updateUseCase() {
+      UseCaseService.Update(vm.usecase)
+        .then(function() {
+          FlashService.Success('Use Case updated');
+
+          //now update the item in the master list. no full refresh needed
+          for (var i = 0; i < vm.usecases.length; i++) {
+            if (vm.usecases[i]._id === vm.usecase._id) {
+              vm.usecases[i] = vm.usecase;
+              break;
+            }
+          }
+        })
+        .catch(function(error) {
+          FlashService.Error(error);
+        });
+    }
+
+    function createUseCase() {
+      UseCaseService.Create(vm.newusecase)
+        .then(function() {
+          FlashService.Success('Use Case created');
+          initController();
+
+          $("#frmNewUsecase")[0].reset();
+
+          //todo refresh
+        })
+        .catch(function(error) {
+          FlashService.Error(error);
+        });
+    }
+
+    function select(usecase) {
+      UseCaseService.GetById(usecase._id).then(function(uc) {
+        vm.usecase = uc;
+      });
+    }
+
+    function deleteUseCase(usecase) {
+      UseCaseService.Delete(usecase._id).then(function() {
+          FlashService.Success('Use Case deleted');
+          $('#confirm-delete').modal('hide');
+          initController();
+
+        })
+        .catch(function(error) {
+          FlashService.Error(error);
+        });
+    }
+  }
 
 })();
--- services/usecase.db.service.js
+++ services/usecase.db.service.js
@@ -4,7 +4,9 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var Q = require('q');
 var mongo = require('mongoskin');
-var db = mongo.db(config.connectionString, { native_parser: true });
+var db = mongo.db(config.connectionString, {
+  native_parser: true
+});
 //var counters = require('mongodb-counter').createCounters({mongoUrl: config.connectionString, collectionName: 'usecases.counter'});
 db.bind('usecases');
 //counters.bind('counters');
@@ -20,155 +22,160 @@ service.getAll = getAll;
 
 module.exports = service;
 
-function getAll()
-{
-    var deferred = Q.defer();
+function getAll() {
+  var deferred = Q.defer();
 
-    db.usecases.find().toArray(function(err, result) {
+  db.usecases.find().toArray(function(err, result) {
 
-        if (err) deferred.reject(err.name + ': ' + err.message);
+    if (err) deferred.reject(err.name + ': ' + err.message);
 
-        if (result) {
-            deferred.resolve(result);
-            //return entries;
-        } else {
-            deferred.resolve();
-        }
-    });
+    if (result) {
+      deferred.resolve(result);
+      //return entries;
+    } else {
+      deferred.resolve();
+    }
+  });
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function getById(_id) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    console.log("getting usecase from DB with ID: " + _id);
+  console.log("getting usecase from DB with ID: " + _id);
 
-    db.usecases.findById(_id, function (err, usecase) {
-        if (err) deferred.reject(err.name + ': ' + err.message);
+  db.usecases.findById(_id, function(err, usecase) {
+    if (err) deferred.reject(err.name + ': ' + err.message);
 
-        if (usecase) {
-            // return user (without hashed password)
-            deferred.resolve(_.omit(usecase, 'hash'));
-        } else {
-            // user not found
-            deferred.resolve();
-        }
-    });
+    if (usecase) {
+      // return user (without hashed password)
+      deferred.resolve(_.omit(usecase, 'hash'));
+    } else {
+      // user not found
+      deferred.resolve();
+    }
+  });
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function create(userParam) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    // validation
-    db.usecases.findOne(
-        { usecasename: userParam.usecasename },
-        function (err, usecase) {
-            if (err) deferred.reject(err.name + ': ' + err.message);
+  // validation
+  db.usecases.findOne({
+      usecasename: userParam.usecasename
+    },
+    function(err, usecase) {
+      if (err) deferred.reject(err.name + ': ' + err.message);
 
-            if (usecase) {
-                // username already exists
-                deferred.reject('usecase "' + userParam.usecasename + '" is already taken');
-            } else {
-                createUseCase();
-            }
+      if (usecase) {
+        // username already exists
+        deferred.reject('usecase "' + userParam.usecasename +
+          '" is already taken');
+      } else {
+        createUseCase();
+      }
+    });
+
+  function createUseCase() {
+    // set user object to userParam without the cleartext password
+    var usecase = _.omit(userParam, 'password');
+
+    console.log("in createUseCase");
+
+    db.usecases.find({}).sort({
+      hid: -1
+    }).limit(1).toArray(function(err, cursor) {
+
+      var myFirstDocument = cursor[0]; //cursor.hasNext() ? cursor.next() : null;
+
+      var nextHID = -1;
+      if (myFirstDocument != null && !isNaN(myFirstDocument.hid)) {
+        //console.log("document is: " + myFirstDocument);
+        console.log("current last usecase is: " + myFirstDocument.usecasename +
+          " with id: " + myFirstDocument.hid);
+        nextHID = myFirstDocument.hid + 1;
+      } else {
+        console.log("no number or entry");
+        nextHID = 1;
+      }
+
+      usecase.hid = nextHID;
+      console.log("created new HID:" + nextHID);
+
+      db.usecases.insert(
+        usecase,
+        function(err, doc) {
+          if (err) {
+            deferred.reject(err.name + ': ' + err.message);
+          }
+
+          deferred.resolve();
+          //break;
         });
+    });
+  }
 
-    function createUseCase() {
-        // set user object to userParam without the cleartext password
-          var usecase = _.omit(userParam, 'password');
-
-          console.log("in createUseCase");
-
-            db.usecases.find({}).sort( { hid: -1 } ).limit(1).toArray( function (err, cursor) {
-
-                  var myFirstDocument = cursor[0]; //cursor.hasNext() ? cursor.next() : null;
-
-                  var nextHID = -1;
-                  if(myFirstDocument != null && !isNaN(myFirstDocument.hid))
-                  {
-                    //console.log("document is: " + myFirstDocument);
-                    console.log("current last usecase is: " + myFirstDocument.usecasename + " with id: " + myFirstDocument.hid);
-                    nextHID = myFirstDocument.hid + 1;
-                  }
-                  else {
-                    console.log("no number or entry");
-                    nextHID = 1;
-                  }
-
-                  usecase.hid = nextHID;
-                  console.log("created new HID:" + nextHID);
-
-                  db.usecases.insert(
-                      usecase,
-                      function (err, doc) {
-                          if (err)
-                          {
-                            deferred.reject(err.name + ': ' + err.message);
-                          }
-
-                          deferred.resolve();
-                          //break;
-                      });
-                  });
-                }
-
-      return deferred.promise;
+  return deferred.promise;
 }
 
 function update(_id, userParam) {
-    var deferred = Q.defer();
-    // fields to update
-    var set = {
-        usecasename: userParam.usecasename,
-        categories: userParam.categories,
-        tags: userParam.tags,
-    };
+  var deferred = Q.defer();
+  // fields to update
+  var set = {
+    usecasename: userParam.usecasename,
+    description: userParam.description,
+    categories: userParam.categories,
+    tags: userParam.tags,
+  };
 
-    // // update password if it was entered
-    // if (userParam.password) {
-    //     set.hash = bcrypt.hashSync(userParam.password, 10);
-    // }
+  // // update password if it was entered
+  // if (userParam.password) {
+  //     set.hash = bcrypt.hashSync(userParam.password, 10);
+  // }
 
-    db.usecases.update(
-        { _id: mongo.helper.toObjectID(_id) },
-        { $set: set },
-        function (err, doc) {
-            if (err) deferred.reject(err.name + ': ' + err.message);
+  db.usecases.update({
+      _id: mongo.helper.toObjectID(_id)
+    }, {
+      $set: set
+    },
+    function(err, doc) {
+      if (err) deferred.reject(err.name + ': ' + err.message);
 
-            deferred.resolve();
-      });
+      deferred.resolve();
+    });
 
-      return deferred.promise;
+  return deferred.promise;
 };
 
 function _delete(_id) {
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    db.usecases.remove(
-        { _id: mongo.helper.toObjectID(_id) },
-        function (err) {
-            if (err) deferred.reject(err.name + ': ' + err.message);
+  db.usecases.remove({
+      _id: mongo.helper.toObjectID(_id)
+    },
+    function(err) {
+      if (err) deferred.reject(err.name + ': ' + err.message);
 
-            deferred.resolve();
-        });
+      deferred.resolve();
+    });
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function _deleteAllUseCases(userid) {
 
-    var deferred = Q.defer();
+  var deferred = Q.defer();
 
-    console.log("deleting usecase from db level");
+  console.log("deleting usecase from db level");
 
-    db.usecases.remove({},function (err) {
-        if (err) deferred.reject(err.name + ': ' + err.message);
+  db.usecases.remove({}, function(err) {
+    if (err) deferred.reject(err.name + ': ' + err.message);
 
-        deferred.resolve();
-    });
+    deferred.resolve();
+  });
 
-    return deferred.promise;
+  return deferred.promise;
 }
